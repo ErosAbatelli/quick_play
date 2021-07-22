9526ad57b92ff074a8a154b52e83d8b247ceca6a
eros <S1090625@studenti.univpm.it>
2021-07-22 16:12:10 +0200

    Commit prova

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dio cinghiale',
+      title: 'Quick Play',
       theme: ThemeData(
         // This is the theme of your application.
         //
