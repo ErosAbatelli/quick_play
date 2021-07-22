7c069ec88c9fbb878eec687db757c461fc874ecc
eros <S1090625@studenti.univpm.it>
2021-07-22 16:19:22 +0200

    Creazione model Utente.dart

---
+++ lib/models/Utente.dart
@@ -0,0 +1,15 @@
+
+class Utente{
+  String name;
+  String surname;
+  String email;
+  String telefono;
+  String password;
+
+  Utente({
+  required this.name,
+    required  this.surname,
+    required this.email,
+    required this.telefono,
+    required this.password });
+}
